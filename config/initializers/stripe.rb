5878d6690d7aaf0e9ae5021b774b5fc89eb3e933
BoonHean <boonhean.low@gmail.com>
2019-04-13 10:40:21 +0800

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
   :publishable_key => 'pk_live_3c20UaORT6Sgitt1ltPeEA1',
-  :secret_key => 'sk_live_AAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+  :secret_key => ENV['ssecret_key']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
